ab53620fba2a6ac416a92f5b66a339e601d7e3c5
Youness Zioual <yzioual@c2r2p6.42lehavre.fr>
2024-10-11 10:17:35 +0200

    fix: leaks

--- src/utils/utils7.c
+++ src/utils/utils7.c
@@ -6,13 +6,13 @@
 /*   By: tblot-la <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 09:57:14 by tblot-la          #+#    #+#             */
-/*   Updated: 2024/10/11 10:08:24 by tblot-la         ###   ########.fr       */
+/*   Updated: 2024/10/11 10:16:16 by yzioual          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-char	**allocate_memory_for_extract_lines(t_game *game, int num_lines)
+char	**allocate_memory_for_extract_lines(int num_lines)
 {
 	char	**lines;
 
