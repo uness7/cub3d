c21e383c5816dfeeeebb0e0e502c85ea17e882d8
Youness Zioual <crness2@gmail.com>
2024-10-14 15:46:14 +0200

    game is finished

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -39,9 +39,9 @@ static int	ft_words_count(const char *s, char delim)
 
 char	**helper(t_split_fun *split, const char *s, char delim)
 {
-	int		i;
-	int		k;;
-	
+	int	i;
+	int	k;
+
 	i = 0;
 	k = -1;
 	split->temp = 0;
@@ -68,6 +68,7 @@ char	**helper(t_split_fun *split, const char *s, char delim)
 char	**ft_split(const char *s, char delim)
 {
 	t_split_fun	split;
+	char		**result;
 
 	if (s == NULL)
 		return (NULL);
@@ -75,27 +76,7 @@ char	**ft_split(const char *s, char delim)
 	split.result = malloc(sizeof(char *) * (split.words_count + 1));
 	if (split.result == NULL)
 		return (NULL);
-	/*
-	split.temp = 0;
-	while (++k < split.words_count)
-	{
-		while (s[i] && (s[i] <= 32 || s[i] == delim))
-			i++;
-		split.start = i;
-		while (s[i] && s[i] > 32 && s[i] != delim)
-			i++;
-		split.word_len = i - split.start;
-		split.result[k] = malloc(sizeof(char) * (split.word_len + 1));
-		if (split.result[k] == NULL)
-			return (NULL);
-		split.temp = -1;
-		while (++split.temp < split.word_len)
-			split.result[k][split.temp] = s[split.start + split.temp];
-		split.result[k][split.temp] = '\0';
-	}
-	split.result[split.words_count] = NULL;
-	*/
-	char **result = NULL; 
+	result = NULL;
 	result = helper(&split, s, delim);
 	return (result);
 }
